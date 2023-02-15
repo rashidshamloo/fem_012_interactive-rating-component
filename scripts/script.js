982dce131da8c1b097863ef97aac75bb4ff887bd
Rashid Shamloo <rashidshamloo@gmail.com>
2023-02-15 05:32:28 +0330

    Updated HTML, CSS and JS
    Added a form containing radio buttons instead of using simple buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,18 @@
-/* variable declarations */
-
-let rating = 0;
-let white = "hsl(0, 0%, 100%)";
-let lightGrey = "hsl(217, 12%, 63%)";
-let mediumGray = "hsl(216, 12%, 25%)";
-
-/* function declarations */
-
-//sets the rating variable based on the innerHTML of the selected rating element
-function setRating() {
-  let newRating = window.event.target.innerHTML;
-  //if the same rating is selected, do nothing
-  if (rating == newRating) {
-    return;
-  } else {
-    rating = newRating;
-  }
-  //reset all other rating element styles to default
-  for (element of document.getElementsByClassName("rating-number-container")[0]
-    .children) {
-    if (element != window.event.target) {
-      element.style.backgroundColor = "";
-      element.style.color = "";
-      element.style.cursor = "";
-    }
-  }
-  //set active style for the selected rating element
-  window.event.target.style.backgroundColor = lightGrey;
-  window.event.target.style.color = white;
-  window.event.target.style.cursor = "default";
-}
+//set event listener for the form
+document.getElementById("rating-form").addEventListener("submit",setRating);
 
 //updates the rating number in DOM and switches the layout divs
-function submitRating() {
+function setRating(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  let rating = formData.get("rating");
   //show message if no rating is selected
-  if (rating == 0) {
+  if (!rating) {
     alert("Please choose a rating before submitting!");
     return;
   }
   //set rating and hide the rating div and show the result div instead
-  document.getElementById("result").innerHTML = rating;
+  document.getElementById("result").textContent = rating;
   document.getElementsByClassName("rating-container")[0].style.display = "none";
   document.getElementsByClassName("result-container")[0].style.display = "flex";
 }
